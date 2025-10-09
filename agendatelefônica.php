7abccd7352f369d72af39068205d2d07a2c5a056
juliaknupp <165339819+juliaknupp@users.noreply.github.com>
2025-10-09 15:23:57 -0300

    Update agendatelefônica.php

--- agendatelefônica.php
+++ agendatelefônica.php
@@ -1,1 +1,51 @@
+<?php
+class AgendaTelefonica {
+    private $contatos = [];
 
+    public function adicionarContato($nome, $telefone) {
+        $this->contatos[$nome] = $telefone;
+        echo "Contato '$nome' adicionado com sucesso!<br>";
+    }
+
+    public function removerContato($nome) {
+        if (isset($this->contatos[$nome])) {
+            unset($this->contatos[$nome]);
+            echo "Contato '$nome' removido com sucesso!<br>";
+        } else {
+            echo "Contato '$nome' não encontrado.<br>";
+        }
+    }
+    public function buscarContato($nome) {
+        if (isset($this->contatos[$nome])) {
+            echo "Telefone de $nome: " . $this->contatos[$nome] . "<br>";
+        } else {
+            echo "Contato '$nome' não encontrado.<br>";
+        }
+    }
+
+    public function listarContatos() {
+        ksort($this->contatos);
+        echo "<h3>Lista de Contatos:</h3>";
+        foreach ($this->contatos as $nome => $telefone) {
+            echo "$nome - $telefone<br>";
+        }
+    }
+}
+
+$agenda = new AgendaTelefonica();
+
+$agenda->adicionarContato("Ana", "9999-1111");
+$agenda->adicionarContato("Carlos", "9888-2222");
+$agenda->adicionarContato("Bruna", "9777-3333");
+
+$agenda->listarContatos();
+echo "<br>";
+
+$agenda->buscarContato("Carlos");
+echo "<br>";
+
+$agenda->removerContato("Ana");
+echo "<br>";
+
+$agenda->listarContatos();
+?>
